b2ec9cde6bd337cf3d1580cae7404884dae36ddb
kvanshika10 <kvanshika4@gmail.com>
2021-07-01 02:37:45 +0530

    Changes made

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 //jshint esversion:6
 const express=require("express");
 const app=express();
+const server=require("http").Server(app);
+const io=require("socket.io")(server);
+const bodyParser=require("body-parser");
+
+
 app.listen(3000,function(){
   console.log("Server started");
 
 });
+app.set('views', __dirname + '/views');
+app.set("view engine","ejs");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+
 app.get("/",function(req,res){
+
    res.sendFile(__dirname+"/index.html");
    console.log("hi");
 });
+
+app.post("/",function(req,res){
+  const room_Id=req.body.id;
+  res.redirect("/"+room_Id);
+  app.get("/"+room_Id,function(request,response){
+
+    response.render("room");
+     });
+});
+
+app.use(express.static("public"));
